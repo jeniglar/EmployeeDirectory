b11c0992f99cf44744921ebc48545d1f7d5e6784
JenIglar <jeniglar@gmail.com>
2020-10-29 15:25:51 -0400

    Added imports to components

---
+++ src/components/employeeTable/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Search from "./search";
+import Header from "./header";
+
+class employeeTable extends Component {
+    state = {
+      result: {},
+      search: ""
+    };
+};
+
+
+export default employeeTable;
