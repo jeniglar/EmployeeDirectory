6c1ba13775c178c85fb01272891c799b500c9a68
JenIglar <jeniglar@gmail.com>
2020-10-30 18:56:23 -0400

    Added preventDefaultand table headings

--- src/components/employeeTable/index.js
+++ src/components/employeeTable/index.js
@@ -5,37 +5,51 @@ import Header from "../Header";
 import employees from "../../employees.json"
 
 class EmployeeTable extends Component {
-    state = {
-      employees: employees,
-      search: "",
-      sort: "ascending"
-    };
+  state = {
+    employees: employees,
+    search: "",
+    sort: "ASC"
+  };
 
-handleInputChange = event => {
-  const { name, value } = event.target;
-  this.setState({
-    [name]: value
-  });
-};
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
-compare(a, b) {
-  if (a.name > b.name) return 1;
-  if (b.name > a.name) return -1;
-  return 0;
-};
-  
-sortOrder = event => {
-  const currentSort = this.state.sort ? "ascending" : "descending";
-  this.setState ({ sort: currentSort });
-  const sortedEmployee = this.state.employees.sort(this.compare);
-  this.setState ({ employees: sortedEmployee })
-};
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      search: ""
+    });
+  };
 
-render () {
+  compare(a, b) {
+    if (a.name > b.name) return 1;
+    if (b.name > a.name) return -1;
+    return 0;
+  };
+
+  sortOrder() {
+    const currentSort = this.state.sort ? "ASC" : "DESC";
+    this.setState({ sort: currentSort });
+    const sortedEmployee = this.state.employees.sort(this.compare);
+    this.setState({ employees: sortedEmployee })
+  };
+
+
+render() {
   return (
     <>
-    <Header/>
-    <Search/>
+      <Header />
+      <Search />
+      <tr className="container">
+        <th >Image</th>
+        <th className="btn" onClick={() => { this.sortOrder() }}>Name</th>
+        <th>Phone Number</th>
+        <th>Email</th>
+      </tr>
     </>
   )
 }
